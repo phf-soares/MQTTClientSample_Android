2ef34c4de4d544d195c942e06eba52b3835bf7d6
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-07-16 22:43:01 -0300

    Pre_fill

--- app/src/main/java/com/example/mqttkotlinsample/MQTTConstants.kt
+++ app/src/main/java/com/example/mqttkotlinsample/MQTTConstants.kt
@@ -5,10 +5,10 @@ const val MQTT_CLIENT_ID_KEY    = "MQTT_CLIENT_ID"
 const val MQTT_USERNAME_KEY     = "MQTT_USERNAME"
 const val MQTT_PWD_KEY          = "MQTT_PWD"
 
-const val MQTT_SERVER_URI       = "tcp://broker.hivemq.com:1883"
-const val MQTT_CLIENT_ID        = ""
-const val MQTT_USERNAME         = ""
-const val MQTT_PWD              = ""
+const val MQTT_SERVER_URI       = "tcp://mqtt3.thingspeak.com:1883"
+const val MQTT_CLIENT_ID        = "ARExBxwdMQw9JzwGMi89JBM"
+const val MQTT_USERNAME         = "ARExBxwdMQw9JzwGMi89JBM"
+const val MQTT_PWD              = "rdASApqPbO5Fq8usVW1ncxia"
 
-const val MQTT_TEST_TOPIC       = "test/topic"
-const val MQTT_TEST_MSG         = "Hello!"
+const val MQTT_TEST_TOPIC       = "channels/3007277/subscribe/fields/field1
+const val MQTT_TEST_MSG         = ""
