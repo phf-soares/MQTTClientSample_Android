a79fb8f9a92dd07f86d23bd6e287bb76e0804d04
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-07-26 22:00:09 -0300

    Update funcional

--- app/src/main/java/com/example/mqttkotlinsample/MQTTConstants.kt
+++ app/src/main/java/com/example/mqttkotlinsample/MQTTConstants.kt
@@ -10,5 +10,5 @@ const val MQTT_CLIENT_ID        = "ARExBxwdMQw9JzwGMi89JBM"
 const val MQTT_USERNAME         = "ARExBxwdMQw9JzwGMi89JBM"
 const val MQTT_PWD              = "rdASApqPbO5Fq8usVW1ncxia"
 
-const val MQTT_TEST_TOPIC       = "channels/3007277/subscribe/fields/field1
+const val MQTT_TEST_TOPIC       = "channels/3007277/subscribe/fields/field1"
 const val MQTT_TEST_MSG         = ""
